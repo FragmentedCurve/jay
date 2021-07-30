2773f76524aad51ffc304daac83feb91a8e925e8
Paco Pascal <me@pacopascal.com>
2021-07-30 05:36:28 -0400

    Fixed the typo that has consistently been realized in many silentbob versions...

--- src/wit.cxx
+++ src/wit.cxx
@@ -18,7 +18,7 @@ int what_is_this (char * d_op, char ch)
 
 	d_words_count = words_count (d_op);
 
-	if (words_count <= 0)
+	if (d_words_count <= 0)
 		return OT::Other;
 	
 	if (d_words_count == 1) {
